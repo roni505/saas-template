7dc4adc39c71f9e0faa7427e699f60f11fe1d743
roni505 <161919194+roni505@users.noreply.github.com>
2025-04-07 02:03:04 +0530

    Analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 const poppins = Poppins({
   subsets: ["latin"], 
@@ -21,9 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable}antialiased`}
+      className={`${poppins.variable}antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
